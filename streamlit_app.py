279be37a5e3ec3f52dfa39f89df6e350adad3d73
vadimbv <57169824+vadimbv@users.noreply.github.com>
2023-01-11 11:07:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Parents' New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -6,3 +9,4 @@ streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-range Egg")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
